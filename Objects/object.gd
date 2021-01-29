857fe67774e1973d5d575d7a757a73c8d0e54029
Santiago Alonso <contresito@gmail.com>
2021-01-29 20:16:13 -0300

    Objetos

---
+++ Objects/object.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+export var keyword_identifier : String
+
+func _process(_delta):
+	z_index = int(position.y)
--- troll.gd
+++ troll.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 const MOTION_SPEED = 160 # Pixels/second.
 
+onready var object_detector = $ObjectDetector
+
 func _physics_process(_delta):
 	var motion = Vector2()
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -10,3 +12,13 @@ func _physics_process(_delta):
 	motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
 	move_and_slide(motion)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("action"):
+		var objects = object_detector.get_overlapping_areas()
+		var nearest_object = objects[0]
+		var object_keyword_identifier = nearest_object.keyword_identifier
+		match object_keyword_identifier:
+			"key": modulate = Color.aqua
+		nearest_object.queue_free()
+		
