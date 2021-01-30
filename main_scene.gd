b3a96a3f29ed45fcd8126dc69b6c75b3c0edfc8f
Claudio Caruzo <cgcaruzo@gmail.com>
2021-01-30 20:56:55 -0300

    en alguna parte del desarrollo de la condicion de victoria

--- main_scene.gd
+++ main_scene.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var timer = $GameEndTimer
 onready var time_left_text = $CanvasLayer/TimeLeftLabel
 onready var game_over_label = $CanvasLayer/GameOverLabel
+onready var victory_label = $CanvasLayer/VictoryLabel
 
 func _ready():
 	SharedVariables.scene = self
