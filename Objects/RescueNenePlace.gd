1346c7387d9d053008206b3802f9bc0481b0a62f
Santiago Alonso <contresito@gmail.com>
2021-01-31 13:41:11 -0300

    Demo completada

--- Objects/RescueNenePlace.gd
+++ Objects/RescueNenePlace.gd
@@ -28,7 +28,7 @@ func _process(_delta):
 	if not nene_rescued and player_inside_range and Input.is_action_just_pressed("action"):
 		print("you touched me!")
 		for object in SharedVariables.player.objects_in_hand:
-			if (object == object_keyword_neccesary and object_keyword_neccesary != "machete") or object_keyword_neccesary == "" :
+			if (object == object_keyword_neccesary and object_keyword_neccesary != "machete" and object_keyword_neccesary != "antorcha") or object_keyword_neccesary == "" :
 				print("you passed")
 				parent.add_child(nene)
 				SharedVariables.scene.add_nenito_to_game(nene)
@@ -36,13 +36,20 @@ func _process(_delta):
 				SharedVariables.player.add_nenito(nene)
 				nene_rescued = true
 				return
-			elif object == "machete" and object_keyword_neccesary == "machete":
-				var tileset = get_parent().get_parent().get_node("Walls")
+			elif (object == "machete" and object_keyword_neccesary == "machete") or (object == "antorcha" and object_keyword_neccesary == "antorcha"):
+				var tileset = get_parent().get_parent().get_node("Walls") if object == "machete" else get_parent().get_node("Walls")
 				var wtm = tileset.world_to_map(position)
 				var cell_id = tileset.get_cellv(wtm)
-				if cell_id == 4:
+				if (cell_id == 4 and object == "machete") or (cell_id == 17 and object == "antorcha"):
 					tileset.set_cellv(wtm,-1)
-					$ObjectSound.play("machete")
+					if object == "machete":
+						$ObjectSound.play("machete")
+					else:
+						$ObjectSound.play("antorcha")
+						parent.add_child(nene)
+						SharedVariables.scene.add_nenito_to_game(nene)
+						nene.position = SharedVariables.player.position + Vector2(10,10)
+						SharedVariables.player.add_nenito(nene)
 					nene_rescued = true
 				return
 		$ShowNegation.play("show_negation_label")
