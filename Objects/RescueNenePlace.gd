70fd6fea4f1b5186d1edc2dcb14a967b329060cb
Santiago Alonso <contresito@gmail.com>
2021-01-31 00:03:28 -0300

    Nene con NodePath

--- Objects/RescueNenePlace.gd
+++ Objects/RescueNenePlace.gd
@@ -3,8 +3,14 @@ extends Area2D
 var player_inside_range := false
 var showed_negation_label := false
 export var object_keyword_neccesary := ""
-export(PackedScene) var nene
+export(NodePath) var nene_path
+onready var nene = get_node(nene_path)
 var nene_rescued := false
+var parent
+
+func _ready():
+	parent = nene.get_parent()
+	parent.remove_child(nene)
 
 func _on_RescueNenePlace_body_entered(body):
 	$ShowIcon.play("show_icon")
@@ -22,10 +28,10 @@ func _process(_delta):
 		for object in SharedVariables.player.objects_in_hand:
 			if object == object_keyword_neccesary:
 				print("you passed")
-				var new_nene = nene.instance()
-				SharedVariables.scene.add_nenito_to_game(new_nene)
-				new_nene.position = SharedVariables.player.position + Vector2(10,10)
-				SharedVariables.player.add_nenito(new_nene)
+				parent.add_child(nene)
+				SharedVariables.scene.add_nenito_to_game(nene)
+				nene.position = SharedVariables.player.position + Vector2(10,10)
+				SharedVariables.player.add_nenito(nene)
 				nene_rescued = true
 				return
 		$ShowNegation.play("show_negation_label")
--- SFX/ClimaBackground/Trueno.gd
+++ SFX/ClimaBackground/Trueno.gd
@@ -7,8 +7,6 @@ onready var truenos = anim_player.get_animation_list()
 
 func _ready():
 	rng.randomize()
-	for i in range(100):
-		print(rng.randi_range(0,truenos.size()-1))
 
 func _on_Timer_timeout():
 	timer.start(rng.randf_range(10,25))
